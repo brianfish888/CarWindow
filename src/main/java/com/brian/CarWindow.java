e2fa2307710785568cb0e0042ff1a8e4bc382041
brian <brian830226@gmail.com>
2021-06-01 15:05:27 +0800

    CarWindow

---
+++ src/main/java/com/brian/CarWindow.java
@@ -0,0 +1,12 @@
+package com.brian;
+
+import javax.swing.*;
+
+public class CarWindow {
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        frame.setSize(800,600);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
